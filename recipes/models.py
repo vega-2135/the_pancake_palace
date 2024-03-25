9829e16195deed7d79078ba35184bed3b50b3b1e
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-03-25 13:50:09 +0100

    Add Recipe model and connect it to the admin dashboard
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+# Recipe post status for admin users
+STATUS = ((0, "Draft"), (1, "Published"))
+
+# Approval status options for recipes submitted by users who are not admin.
+APPROVAL_STATUS = (
+    ('0', 'Pending Approval'),
+    ('1', 'Approved'),
+    ('2', 'Rejected'),
+)
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="resources_added")
+    servings = models.IntegerField(
+        help_text='Please enter the number of portions that result from your recipe.'
+    )
+    cooking_duration = models.IntegerField(
+        help_text = 'Please enter how many minutes is required for the preparation of your recipe')
+    ingredients = models.TextField()
+    preparation = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    approval_status = models.IntegerField(choices=APPROVAL_STATUS, default=0)
+    tags = TaggableManager(blank=True)
+    recipe_image = CloudinaryField('image', default='placeholder')
+    likes = models.ManyToManyField(
+        User, related_name='recipe_likes', blank=True)
+    stars = models.ManyToManyField(
+        User, related_name='recipe_stars', blank=True)
+    
--- the_pancake_palace/settings.py
+++ the_pancake_palace/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-96na^be%v$ewlxkd^!u6=!7gn!_k9l#rt@bc6v@)2gfq=z*3-w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".herokuapp.com",]
+ALLOWED_HOSTS = ["http://0.0.0.0:8000/", "127.0.0.1", ".herokuapp.com"]
 
 
 # Application definition
@@ -41,7 +41,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cloudinary_storage",
+    "cloudinary",
+    "taggit",
     "recipes",
+    "about"
 ]
 
 MIDDLEWARE = [
@@ -88,6 +92,11 @@ WSGI_APPLICATION = "the_pancake_palace.wsgi.application"
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
