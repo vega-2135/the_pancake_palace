4124102b35692b68d07cc9f1bdc5611e82331cf7
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-04-05 13:26:52 +0200

    Add functionality to edit comments by users
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,4 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
     path('<slug:slug>/', views.recipe_detail, name='recipe_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from .models import Recipe
+from django.http import HttpResponseRedirect
+from .models import Recipe, Comment
 import re
 from .forms import CommentForm
 from .forms import RatingForm
@@ -103,3 +104,26 @@ def rate_recipe(request, recipe_id):
         form = RatingForm()
     
     return render(request, 'rate_recipe.html', {'form': form})
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    Function to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.recipe = recipe
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
