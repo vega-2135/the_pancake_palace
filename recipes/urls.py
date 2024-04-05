282a8985f13b4c0c04ec0a60ea6c5dff7d78d769
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-04-05 13:41:43 +0200

    Add functionality fot deliting comments by users
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('<slug:slug>/', views.recipe_detail, name='recipe_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>',
          views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -127,3 +127,20 @@ def comment_edit(request, slug, comment_id):
             messages.add_message(request, messages.ERROR, 'Error updating comment!')
 
     return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
+
+
+def comment_delete(request, slug, comment_id):
+    """
+    view to delete comment
+    """
+    queryset = Recipe.objects.filter(status=1)
+    recipe = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
