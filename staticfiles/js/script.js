8a04f6f20afc3310745d826e97a53c54fbb641a7
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-04-16 14:15:50 +0200

    Style 404 page
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,28 +1,8 @@
 // Get the alert element
-const alertElement = document.getElementById('msg');
+const alertElement = document.getElementById("msg");
 
-// Automatically dismiss the alert after 3 seconds (adjust the time as needed)
+// Automatically dismiss the alert after 2 seconds
 setTimeout(function() {
   alertElement.classList.remove('show');
   alertElement.classList.add('hide');
-}, 3000); // 3000 milliseconds = 3 seconds
-
-// function highlightStars(selectedStar) {
-//   for (let starIndex = 1; starIndex <= 5; starIndex++) {
-//       const star = document.getElementById('star' + starIndex);
-//       const label = document.querySelector('label[for="star' + starIndex + '"]');
-//       if (starIndex <= selectedStar) {
-//           label.style.color = '#ffca08'; 
-//       } else {
-//           label.style.color = ''; // Reset color to default
-//       }
-//   }
-// }
-
-// function highlightStars(selectedStar) {
-//   for (let i = selectedStar; i <= 0; i--){
-//     const label = document.querySelector(`label[for="star${i}"]`)
-//     label.style.color = '#ffca08'; 
-//   }
-
-// }
+}, 2000); 
--- the_pancake_palace/settings.py
+++ the_pancake_palace/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get("DEBUG_DJANGO") else False
 
-ALLOWED_HOSTS = ["http://0.0.0.0:8000/", "127.0.0.1", ".herokuapp.com"]
+ALLOWED_HOSTS = ["http://0.0.0.0:8000/", "127.0.0.1", ".herokuapp.com", "localhost"]
 
 
 # Application definition
