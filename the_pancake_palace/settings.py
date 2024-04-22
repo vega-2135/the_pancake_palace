09dcbd26c7e661e29a57b7877918d5dd776e2ec1
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-04-22 13:21:03 +0200

    Set up testing and add tests for comment form
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

--- the_pancake_palace/settings.py
+++ the_pancake_palace/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -129,6 +130,11 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+    
+
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
     "https://*.herokuapp.com"
@@ -213,11 +219,6 @@ LOGGING = {
         },
     }
 }
-
-MESSAGE_TAGS = {
-   messages.WARNING: 'alert-warning',
-}
-
 
 SUMMERNOTE_CONFIG = {
     # You can put custom Summernote settings
