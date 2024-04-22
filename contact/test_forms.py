9894b65586ba8e6387f5f0f5c4990529ab181d00
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-04-22 13:22:05 +0200

    Add tests for reach out form from contact page
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

---
+++ contact/test_forms.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from .forms import ReachOutForm
+
+
+class TestCollaborateForm(TestCase):
+
+    def test_form_is_valid(self):
+        """ Test for all fields"""
+        form = ReachOutForm({
+            'name': 'test',
+            'email': 'test@test.com',
+            'message': 'Hello!'
+        })
+        self.assertTrue(form.is_valid(), msg="Form is not valid")
+
+    def test_name_is_required(self):
+        """ Test for all fields"""
+        form = ReachOutForm({
+            'name': '',
+            'email': 'test@test.com',
+            'message': 'Hello!'
+        })
+        self.assertFalse(form.is_valid(), msg="Name field is empty but the form is valid")
+
+    def test_email_is_required(self):
+        """ Test for all fields"""
+        form = ReachOutForm({
+            'name': 'test',
+            'email': '',
+            'message': 'Hello!'
+        })
+        self.assertFalse(form.is_valid(), msg="Email field is empty but the form is valid"")
+
+    def test_message_is_required(self):
+        """ Test for all fields"""
+        form = ReachOutForm({
+            'name': 'test',
+            'email': 'test@test.com',
+            'message': ''
+        })
+        self.assertFalse(form.is_valid(), msg="Message field is empty but the form is valid"")
