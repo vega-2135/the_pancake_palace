a1408f7560bc1547f5312bc05a3634c968adc2cc
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-04-26 12:37:31 +0200

    test_views: Add tests for ShareRecipes
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

--- recipes/test_views.py
+++ recipes/test_views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.test import Client, TestCase
+from django.urls import reverse
 
 from .models import Recipe
 
@@ -71,4 +72,44 @@ class TestRecipeListView(TestCase, Client):
         ).order_by('-created_on')
         self.assertQuerysetEqual(test_qs, expected_qs)
 
+
+class TestShareRecipeView(TestCase, Client):
+
+    def setUp(self):
+        self.user = User.objects.create_superuser(
+            username="myUsername", password="myPassword", email="test@test.com"
+        )
+
+    def test_no_access_to_create_recipe_if_not_logged_in(self):
+        response = self.client.get(reverse('share_recipe'))
+        self.assertRedirects(response, '/accounts/login/?next=/share/')
+      
+    def test_authenticated_user_can_access_share_recipe_page(self):
+        user = User.objects.get(pk=1)
+        self.client.force_login(user=user)
+        response = self.client.get('/share/')
+        self.assertContains(response, 'title')
+        self.assertContains(response, 'cooking_duration')
+        self.assertContains(response, 'servings')
+        self.assertContains(response, 'category')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'share_recipe.html')
+      
+    def test_create_recipe(self):
+        user = User.objects.get(pk=1)
+        self.client.force_login(user=user)
+        response = self.client.post('/share/', {
+            'title': 'Recipe Title',
+            'cooking_time': 10,
+            'serves': 1,
+            'ingredients': '1 cup flour, 1cup milk',
+            'preparation':'Step 1: Mix all together.',
+            'category': 1,
+            'make_publik': 'True'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '/share/')
+        self.assertEqual(Recipe.objects.count(), 1)
+
+    
 
