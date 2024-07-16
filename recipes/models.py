16f942e3190909645ea05a7a69eb2c1b9e91efa7
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-07-16 12:34:07 +0200

    Add Docstrings to recipe model in recipe app
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

--- recipes/models.py
+++ recipes/models.py
@@ -16,6 +16,63 @@ CATEGORY = (
 
 # Recipe Model
 class Recipe(models.Model):
+    """
+    Model representing a recipe.
+
+    This model contains all the necessary fields to describe a recipe,
+    including title, author, servings, cooking duration, ingredients,
+    preparation steps, and additional metadata like creation and update
+    timestamps, status, category, image, likes, and ratings.
+
+    Attributes:
+    -----------
+    title : str
+        The title of the recipe. Must be unique.
+    slug : str
+        A unique slug automatically generated from the title.
+    author : ForeignKey
+        The user who created the recipe.
+    servings : int
+        The number of portions the recipe yields.
+    cooking_duration : int
+        The cooking duration in minutes.
+    ingredients : str
+        The ingredients required for the recipe.
+    preparation : str
+        The preparation steps for the recipe.
+    created_on : datetime
+        The date and time when the recipe was created.
+    updated_on : datetime
+        The date and time when the recipe was last updated.
+    status : int
+        The publication status of the recipe.
+    category : int
+        The category of the recipe.
+    recipe_image : CloudinaryField
+        The image of the recipe.
+    saved : ManyToManyField
+        Users who have saved the recipe.
+    likes : ManyToManyField
+        Users who have liked the recipe.
+    saved_boolean : bool
+        Boolean flag indicating if the recipe has been saved.
+    rating : int
+        The rating of the recipe.
+    number_of_ratings : int
+        The number of ratings the recipe has received.
+    approved : bool
+        Boolean flag indicating if the recipe is approved.
+    make_public : bool
+        Boolean flag indicating if the recipe should be published online.
+
+    Methods:
+    --------
+    __str__():
+        Returns the string representation of the recipe.
+    likes_count():
+        Returns the count of likes for the recipe.
+    """
+
     title = models.CharField(max_length=200, unique=True)
     slug = AutoSlugField(populate_from="title", unique=True)
     author = models.ForeignKey(
@@ -51,21 +108,64 @@ class Recipe(models.Model):
     approved = models.BooleanField(default=False)
     make_public = models.BooleanField(
         default=False,
-        help_text="Check this box to allow your recipe to be published online.",
+        help_text="Check this box to allow your recipe to"
+        "be published online.",
     )
 
     class Meta:
         ordering = ["-created_on"]
 
     def __str__(self):
+        """
+        Returns the string representation of the recipe, which is the title.
+        """
         return self.title
 
     def likes_count(self):
+        """
+        Returns the count of likes for the recipe.
+        """
         return self.likes.count()
 
 
 # Comment Model
 class Comment(models.Model):
+    """
+    Model representing a comment on a recipe.
+
+    This model contains fields to describe a comment made by a user on a
+    recipe,including the content of the comment, the user who made the comment,
+    the recipe being commented on, and metadata like creation time, approval
+    status and likes.
+
+    Attributes:
+    -----------
+    recipe : ForeignKey
+        The recipe that the comment is associated with.
+    author : ForeignKey
+        The user who made the comment.
+    content : str
+        The content of the comment.
+    likes : ManyToManyField
+        Users who have liked the comment.
+    created_on : datetime
+        The date and time when the comment was created.
+    approved : bool
+        Boolean flag indicating if the comment is approved.
+
+    Methods:
+    --------
+    __str__():
+        Returns the string representation of the comment.
+    recipe_title():
+        Returns the title of the recipe associated with the comment.
+    recipe_rating():
+        Returns the rating of the recipe associated with the comment.
+    recipe_number_of_ratings():
+        Returns the number of ratings of the recipe associated with the
+        comment.
+    """
+
     recipe = models.ForeignKey(
         Recipe, on_delete=models.CASCADE, related_name="comments"
     )
@@ -86,13 +186,27 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
+        """
+        Returns the string representation of the comment, including its content
+        and the author.
+        """
         return f"Comment {self.content} by {self.author}"
 
     def recipe_title(self):
+        """
+        Returns the title of the recipe associated with the comment.
+        """
         return self.recipe.title
 
     def recipe_rating(self):
+        """
+        Returns the rating of the recipe associated with the comment.
+        """
         return self.recipe.rating
 
     def recipe_number_of_ratings(self):
+        """
+        Returns the number of ratings of the recipe associated with the
+        comment.
+        """
         return self.recipe.number_of_ratings
