6149395c044813f94276314f30b6bc0681a9159e
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-09-17 14:45:16 +0200

    Fix image file validation part 2
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

--- static/js/checkFileType.js
+++ static/js/checkFileType.js
@@ -1,9 +1,21 @@
 /*jshint esversion: 6 */
-document.getElementById("img-ext").addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    if (file.size > 1258291) { 
-        alert('Please upload an image smaller than 1.2 MiB.');
-        // Clear the input field or prevent further actions
-        event.target.value = '';
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("img-ext").addEventListener("change", function(event) {
+        var file = event.target.files[0];
+        if (file.size > 1258291) {
+            alert("Please upload an image smaller than 1.2 MiB.");
+            // Clear the input field or prevent further actions
+            event.target.value = '';
+        }
+
+        var fileType = file.type;
+
+        // Allowed MIME types for PNG and JPG
+        var allowedTypes = ["image/png", "image/jpeg", "image/webp"];
+
+        if (!allowedTypes.includes(fileType)) {
+            alert("Only PNG, JPG or WEBP files are allowed.");
+            event.preventDefault();
+        }
+    });
 });
