c639db9cd752cc626554d98f89ef681829f0b3be
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-09-20 15:04:22 +0200

    Improve recipe detail page UX for small devices
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

---
+++ staticfiles/js/recipes.0ae1ad0af7c2.js
@@ -0,0 +1,31 @@
+/*jshint esversion: 6 */
+const deleteRecipeModal = new bootstrap.Modal(document.getElementById("deleteRecipeModal"));
+const deleteRecipeButton = document.getElementsByClassName("delete-recipe");
+const deleteRecipeConfirm = document.getElementById("deleteRecipeConfirm");
+const closeModalButtons = document.getElementsByClassName("modal-close-btn");
+
+
+/**
+* Initializes deletion functionality for the provided delete buttons.
+* 
+* For each button in the `deleteButtons` collection:
+* - Retrieves the associated comment's ID upon click.
+* - Updates the `deleteConfirm` link's href to point to the 
+* deletion endpoint for the specific comment.
+* - Displays a confirmation modal (`deleteModal`) to prompt 
+* the user for confirmation before deletion.
+*/
+
+for (let button of deleteRecipeButton) {
+    button.addEventListener("click", (e) => {
+      let recipeId = e.target.getAttribute("recipe_id");
+      deleteRecipeConfirm.href = `delete_submitted_recipe/${recipeId}`;
+      deleteRecipeModal.show();
+    });
+  }
+
+  for (let button of closeModalButtons) {
+    button.addEventListener("click", (e) => {
+      deleteRecipeModal.hide();
+    });
+  }
---
+++ staticfiles/js/script.9e143d664697.js
@@ -0,0 +1,10 @@
+/*jshint esversion: 6 */
+// Get the alert element
+const alertElement = document.getElementById("msg");
+
+// Automatically dismiss the alert after 2 seconds
+setTimeout(function() {
+  alertElement.classList.remove('show');
+  alertElement.classList.add('hide');
+}, 3000); 
+
